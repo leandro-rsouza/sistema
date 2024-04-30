a8acab3c3ea78dfa82216256387d968f4d58699c
unknown <leandro.rochasouza98@gmail.com>
2024-04-30 11:02:24 -0300

    Tabela de Salas Online

--- room.php
+++ room.php
@@ -7,32 +7,47 @@
     <title> Salas </title>
 </head>
 
-<?php
-    require_once 'vendor/autoload.php';
-    $consultar = new universe\backend\Crud();
+<body>
+    <section>
+        <?php
+        require_once 'vendor/autoload.php';
 
-    foreach($consultar->read_game() as $sala):
-    echo
-    " 
-    <body>
-        <section>
-            <table border='1' style='text-align: center'>
-                <tr>
-                    <td> Número da Sala </td>
-                    <td> Nº de Jogadores </td>
-                    <td> Opção </td>
-                </tr>
-                <tr>
-                    <td> ".$sala['id_partida']." </td>
-                    <td> ".$sala['num_players']." </td>
-                    <td> <a href='online-game.php?set=visitante&id=".$sala['id_partida']."&visitante=".$_SESSION['id']."'> Entrar </a> </td>
-                </tr>
-            </table>
-        </section>
-        
-        </body>
+        $consultar = new universe\backend\Crud();
+        $sala = $consultar->read_game();
+
+        echo
+        " 
+        <table border='1' style='text-align: center'>
+            <tr>
+                <td> Número da Sala </td>
+                <td> Nº de Jogadores </td>
+                <td> Opção </td>
+            </tr>
         ";
+
+        if(count($sala) == 0):
+            echo "<tr> <td colspan='3'> Nenhuma Sala Disponível! </td> </tr>";
+        endif;
+
+        foreach($consultar->read_game() as $sala):
+        echo 
+        "
+        <tr>
+            <td> ".$sala['id_partida']." </td>
+            <td> ".$sala['num_players']."/2 </td>"; 
+            if($sala['num_players'] == 2):
+                echo " <td> <span class='msg-sala'> Cheia </span> </td>";
+            else:
+                echo "<td> <a href='online-game.php?set=visitante&id=".$sala['id_partida']."&visitante=".$_SESSION['id']."'> Entrar </a> </td>";
+            endif;
+        "</tr>";
         endforeach;
-    echo "<div class='btn-create-room'> <a href='online-game.php?set=mandante&id=".$_SESSION['id']."&nome=".$_SESSION['name']."'> Criar Sala </a> </div>";
-    ?>
+
+        echo    
+        "</table>
+        
+        <div class='btn-create-room'> <a href='online-game.php?set=mandante&id=".$_SESSION['id']."&nome=".$_SESSION['name']."'> Criar Sala </a> </div>";
+        ?>
+    </section>
+</body>
 </html>
--- universe.sql
+++ universe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 11-Jan-2024 às 13:09
+-- Tempo de geração: 30-Abr-2024 às 12:27
 -- Versão do servidor: 10.4.10-MariaDB
 -- versão do PHP: 7.3.12
 
@@ -25,6 +25,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `partida`
+--
+
+DROP TABLE IF EXISTS `partida`;
+CREATE TABLE IF NOT EXISTS `partida` (
+  `id_partida` int(11) NOT NULL AUTO_INCREMENT,
+  `num_players` int(11) NOT NULL,
+  `fk_player1` int(11) DEFAULT NULL,
+  `fk_player2` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_partida`),
+  KEY `fk_player1` (`fk_player1`),
+  KEY `fk_player2` (`fk_player2`)
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `partida`
+--
+
+INSERT INTO `partida` (`id_partida`, `num_players`, `fk_player1`, `fk_player2`) VALUES
+(16, 2, 11, 11);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `pessoa`
 --
 
@@ -32,11 +56,11 @@ DROP TABLE IF EXISTS `pessoa`;
 CREATE TABLE IF NOT EXISTS `pessoa` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
-  `contato` int(11) NOT NULL,
+  `contato` bigint(20) NOT NULL,
   `email` varchar(50) NOT NULL,
   `senha` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `pessoa`
@@ -51,7 +75,8 @@ INSERT INTO `pessoa` (`id`, `nome`, `contato`, `email`, `senha`) VALUES
 (8, 'José Henrique', 981242414, 'henrique@gmail.com', 321),
 (13, 'Gustavo Santos', 981145563, 'gustavo@gmail.com', 123),
 (14, 'Fernando Souza', 981155632, 'fernando@gmail.com', 123),
-(15, 'Ricardo Santos', 981156520, 'ricardo@gmail.com', 123);
+(15, 'Ricardo Santos', 981156520, 'ricardo@gmail.com', 123),
+(16, 'Miguel Silva', 981156332, 'miguel@gmail.com', 123);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
